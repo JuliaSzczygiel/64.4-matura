e6f2a5dedbe6ce80dee9cac63b81e88488097b93
JuliaSzczygiel <62343530+JuliaSzczygiel@users.noreply.github.com>
2021-03-18 13:31:22 +0100

    Update 64.4.cpp

--- 64.4.cpp
+++ 64.4.cpp
@@ -43,8 +43,8 @@ void file::loop()
 {
     int tab[20][20];
     int a;
-    int Row = 0;
-    int Column = 0;
+    int Row=0;
+    int Column=0;
     int sum;
     string row[21];
     string column[21];
@@ -61,7 +61,7 @@ void file::loop()
                 {
                     inFile >> column[j];
 
-                    if (row[i][j] & column[j][i] == 0) 
+                    if (row[i][j] & column[j][i]==0) 
                     {
                         inFile >> sum;
                     }
@@ -71,17 +71,17 @@ void file::loop()
                     {
                         for(int d=0; d<=21; d++) 
                         {
-                            if(row & column % 2 == 0) 
+                            if(row & column % 2==0) 
                             {
                                 int sum = 0;
 
-                                if((sum%2 == 0 && row[i][20] !='0') || (sum%2 == 1 && row[i][20] =='1')) 
+                                if((sum%2==0 && row[i][20] !='0') || (sum%2==1 && row[i][20]=='1')) 
                                 { 
                                     cout << Row << "," << '\n';
                                     Row++;
                                 }
 
-                                if((sum%2 == 0 && column[j][20] !='0') || (sum%2 == 1 && column[j][20] =='1')) 
+                                if((sum%2==0 && column[j][20] !='0') || (sum%2==1 && column[j][20]=='1')) 
                                 {
                                     cout << Column << "," << '\n';
                                     Column++;
